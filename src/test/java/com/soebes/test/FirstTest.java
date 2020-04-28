ef9fd183c3f11f0a51ecc777d9c40c75ceaa188e
Karl Heinz Marbaise <kama@soebes.de>
2020-04-28 19:39:32 +0200

    First.

---
+++ src/test/java/com/soebes/test/FirstTest.java
@@ -0,0 +1,13 @@
+package com.soebes.test;
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class FirstTest {
+
+  @Test
+  void first_test_method() {
+    assertThat(true).isTrue();
+  }
+}
